6ce44d62aa52534429be6efe650c2baf56b89c2d
Alessandro Braidotti <alessandro.braidotti@gmail.com>
2018-01-30 16:25:20 -0500

    add username and password handler

--- index.js
+++ index.js
@@ -2,3 +2,15 @@ const chalk = require('chalk');
 const clear = require('clear');
 const figlet = require('figlet');
 const files = require('./lib/files');
+
+clear();
+console.log(
+  chalk.yellow(
+    figlet.textSync('Ginit', { horizontalLayout: })
+  )
+);
+
+if (files.directoryExists('.git')){
+  console.log(chalk.red('Already a git repository!'));
+  process.exit();
+}
