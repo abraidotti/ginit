b33b3d7507539ca3e0429d30e1ec14f218caf39e
Alessandro Braidotti <alessandro.braidotti@gmail.com>
2018-02-03 16:06:32 -0500

    update run function

--- index.js
+++ index.js
@@ -17,12 +17,36 @@ if (files.directoryExists('.git')){
 }
 
 const run = async () => {
-  let token = github.getStoredGithubToken();
-  if(!token) {
-    await github.setGithubCredentials();
-    token = await github.registerNewToken();
+  try {
+    // Retrieve & Set Authentication Token
+    const token = await getGithubToken();
+    github.githubAuth(token);
+
+    // Create remote repository
+    const url = await repo.createRemoteRepo();
+
+    // Create .gitignore file
+    await repo.createGitignore();
+
+    // Set up local repository and push to remote
+    const done = await repo.setupRepo(url);
+    if(done) {
+      console.log(chalk.green('All done!'));
+    }
+  } catch(err) {
+      if (err) {
+        switch (err.code) {
+          case 401:
+            console.log(chalk.red('Couldn\'t log you in. Please provide correct credentials/token.'));
+            break;
+          case 422:
+            console.log(chalk.red('There already exists a remote repository with the same name'));
+            break;
+          default:
+            console.log(err);
+        }
+      }
   }
-  console.log(token);
 }
 
 const getGithubToken = async () => {
